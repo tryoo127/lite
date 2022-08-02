74cf2937191c4fbdaf673b84bf43adefe1d1e07e
tryoo127 <tryoo127@gmail.com>
2022-08-02 22:13:04 +0800

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -90,33 +90,15 @@ echo -e "\e[33m ━━━━━━━━━━━━━━━━━━━━━�
 echo -e " \E[0;100;33m        • MULTIPORT SCRIPT LITE BY TRYOO •            \E[0m"
 echo -e "\e[33m ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
 echo ""
-echo "   >>> Service & Port"  | tee -a log-install.txt
+echo "   >>> Service & Port"               | tee -a log-install.txt
 echo "   - XRAY VLESS XTLS SPLICE  : 443"  | tee -a log-install.txt
 echo "   - XRAY VLESS XTLS DIRECT  : 443"  | tee -a log-install.txt
 echo "   - XRAY VLESS WS TLS       : 443"  | tee -a log-install.txt
 echo "   - XRAY TROJAN TLS         : 443"  | tee -a log-install.txt
 echo "   - XRAY VMESS TLS          : 443"  | tee -a log-install.txt
-echo "   - V2RAY VLESS NONE TLS    : 80" | tee -a log-install.txt
-echo ""  | tee -a log-install.txt
-echo "   >>> Server Information & Other Features"  | tee -a log-install.txt
-echo "   - Timezone                 : Asia/Kuala_Lumpur (GMT +8)"  | tee -a log-install.txt
-echo "   - Fail2Ban                 : [ON]"  | tee -a log-install.txt
-echo "   - DDOS Dflate              : [ON]"  | tee -a log-install.txt
-echo "   - IPtables                 : [ON]"  | tee -a log-install.txt
-echo "   - Auto-Reboot              : [OFF]" | tee -a log-install.txt
-echo "   - IPv6                     : [OFF]" | tee -a log-install.txt
-echo "   - Auto-Remove-Expired      : [ON]"  | tee -a log-install.txt
-echo "   - Autobackup Data                "  | tee -a log-install.txt
-echo "   - AutoKill Multi Login User       " | tee -a log-install.txt
-echo "   - Auto Delete Expired Account     " | tee -a log-install.txt
-echo "   - Fully automatic script          " | tee -a log-install.txt
-echo "   - VPS settings                    " | tee -a log-install.txt
-echo "   - Admin Control                   " | tee -a log-install.txt
-echo "   - Change port                     " | tee -a log-install.txt
-echo "   - Restore Data                    " | tee -a log-install.txt
-echo "   - Full Orders For Various Services" | tee -a log-install.txt
+echo "   - V2RAY VLESS NONE TLS    : 80"   | tee -a log-install.txt
 echo ""
-echo -e "\e[33m━━━━━━━━━[\e[0m \e[32mSayang Ibu Ayah\e[0m \e[33m]━━━━━━━━━━━\e[0m"
+echo -e "\e[33m━━━━━━━[\e[0m \e[32mSayang Ibu Ayah\e[0m \e[33m]━━━━━━━━━\e[0m"
 echo ""
 echo -e ""
 sleep 3
